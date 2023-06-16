ef0a641805d42b767a731779acd46670054f3a51
JamoDevy <devinlclarke@gmail.com>
2023-06-15 23:20:30 -0400

    writing jQuery

--- app/javascript/src/index.js
+++ app/javascript/src/index.js
@@ -11,13 +11,28 @@ import {
 indexTasks(function (response) {
   var htmlString = response.tasks.map(function(task) {
     return "<div class='col-12 mb-3 p-2 border rounded task' data-id='" + task.id + "'> \
-      " + task.content + "\  <button id='delete' classname='btn btn-primary mb-2' style='float:right'>Remove</button> \
-      </div>";
-
+      " + task.content + "\ <button id='remove' classname='btn btn-primary mb-2' style='float:right'>Remove</button> \
+      </div>"
+      
   });
 
   $("#tasks").html(htmlString);
+
+  $('#submit').on("click", function () {
+    postTask($("#task-input"));
+  });
+
+  $('#remove').on('click', function(e) {
+    console.log("this removes");
+    e.preventDefault();
+  });
+
 });
+
+
+
+
+
 
 
 
