f94693222aca400b9bfbd79314dd8789321eeee2
JamoDevy <devinlclarke@gmail.com>
2023-06-24 21:03:46 -0400

    rough draft of assignment

--- app/javascript/src/index.js
+++ app/javascript/src/index.js
@@ -11,32 +11,48 @@ import {
 indexTasks(function (response) {
   var htmlString = response.tasks.map(function(task) {
     return "<div class='col-12 mb-3 p-2 border rounded task' data-id='" + task.id + "'> \
-      " + task.content + "\ <button id='remove' classname='btn btn-primary mb-2' style='float:right'>Remove</button> \
+      " + task.content + "\ <input type='checkbox' id='taskCheckBox' style='float:right'><button id='remove' classname='btn btn-primary mb-2' style='float:right'>Remove</button> \
       </div>"
       
   });
 
   $("#tasks").html(htmlString);
 
-  $('#submit').on("click", function () {
-    postTask($("#task-input"));
+  $('#submit').on("click", function (e) {
+    e.preventDefault();
+    postTask($("task-input"));
   });
 
   $('#remove').on('click', function(e) {
-    console.log("this removes");
+    deleteTask();
     e.preventDefault();
   });
+  
 
+
+markTaskActive(function (task) {
+  
+  if ($('#taskCheckBox') == checked)
+      task.complete == false
+    
+  else 
+    ($('#taskCheckBox') == unchecked) 
+      task.complete == true
 });
 
+markTaskComplete(function (response) {
+  var htmlString = response.tasks.map(function(task) {
+    if (task.complete == true)
+      return "<div class='col-12 mb-3 p-2 border rounded task' data-id='" + task.complete + "'> \
+      " + task.content + "\
+      </div>"
+    else (task.complete == false)
 
 
+  })
+
+$('#active').html(htmlString)
 
 
-
-
-
-
-
-
-
+});
+});
