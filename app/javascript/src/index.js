3aabc8f1c5dd01d887e3aac6dfdf94ee827fcd0e
h8rry <harrychenca@gmail.com>
2023-06-26 16:54:33 +0800

    Able to post a task and refresh the page

--- app/javascript/src/index.js
+++ app/javascript/src/index.js
@@ -1,4 +1,4 @@
-import $ from 'jquery';
+import $ from "jquery";
 
 import {
   indexTasks,
@@ -8,51 +8,59 @@ import {
   deleteTask,
 } from "./requests.js";
 
-indexTasks(function (response) {
-  var htmlString = response.tasks.map(function(task) {
-    return "<div class='col-12 mb-3 p-2 border rounded task' data-id='" + task.id + "'> \
-      " + task.content + "\ <input type='checkbox' id='taskCheckBox' style='float:right'><button id='remove' classname='btn btn-primary mb-2' style='float:right'>Remove</button> \
-      </div>"
-      
-  });
-
-  $("#tasks").html(htmlString);
-
-  $('#submit').on("click", function (e) {
-    e.preventDefault();
-    postTask($("task-input"));
-  });
-
-  $('#remove').on('click', function(e) {
-    deleteTask();
-    e.preventDefault();
-  });
+function refreshTasksList() {
+  indexTasks(function (response) {
+    var htmlString = response.tasks.map(function (task) {
+      return (
+        "<div class='col-12 mb-3 p-2 border rounded task' data-id='" +
+        task.id +
+        "'> \
+        " +
+        task.content +
+        " \
+        <input type='checkbox' id='taskCheckBox' style='float:right'> \
+        <button id='remove' class='btn btn-primary mb-2' style='float:right'>Remove</button> \
+        </div>"
+      );
+    });
   
+    $("#tasks").html(htmlString);
+  });
+}
 
 
-markTaskActive(function (task) {
+// Wait for the document to be ready before executing the code inside the function
+$(document).ready(function () {
+
+  // Call the refreshTasksList function to display the list of tasks
+  refreshTasksList();
+
+  // When the submit button is clicked, prevent the default form submission behavior
+  $("#submit").on("click", function(e) {
+    e.preventDefault();
   
-  if ($('#taskCheckBox') == checked)
-      task.complete == false
-    
-  else 
-    ($('#taskCheckBox') == unchecked) 
-      task.complete == true
-});
+    // Log the value of the task input field to the console
+    console.log($("#task-input").val());
 
-markTaskComplete(function (response) {
-  var htmlString = response.tasks.map(function(task) {
-    if (task.complete == true)
-      return "<div class='col-12 mb-3 p-2 border rounded task' data-id='" + task.complete + "'> \
-      " + task.content + "\
-      </div>"
-    else (task.complete == false)
+    // Call the postTask function to add a new task to the list and refresh the list
+    postTask($("#task-input").val(), function (response) {
+      refreshTasksList();
+    });
+  });
 
+  // When the remove button is clicked, call the deleteTask function to remove the task from the list
+  // $("#remove").on("click", function (e) {
+  //   deleteTask();
+  //   e.preventDefault();
+  // });
+  
+  // When the checkbox is clicked, call the markTaskActive function to mark the task as active or complete
+  // markTaskActive(function (task) {
+  //   if ($("#taskCheckBox").is(":checked")) {
+  //     task.complete = false;
+  //   } else {
+  //     task.complete = true;
+  //   }
+  // });
+})
 
-  })
-
-$('#active').html(htmlString)
-
-
-});
-});
